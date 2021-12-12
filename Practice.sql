7d0f11dec554ef919626c046e271a3cabd05919e
vevdokimenko <evdokimenko@hotmail.com>
2021-12-12 23:33:17 +0200

    Practice

--- Practice.sql
+++ Practice.sql
@@ -39,13 +39,62 @@ BEGIN
     FROM Books
              JOIN Authors A ON A.ID_AUTHOR = Books.ID_AUTHOR
     WHERE A.FirstName = authorName;
-END; |
+END;
+|
 
 CALL getBooksOfAuthor('John');
 
 
-
 # 4. Функцию, которая возвращает самую дорогую книгу указанной тематики.
+CREATE FUNCTION getExpensiveBookByTheme(theme VARCHAR(30))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    RETURN (
+        SELECT ID_BOOK
+        FROM Books
+        WHERE Price IN (
+            SELECT MAX(Price)
+            FROM Books
+                     JOIN Themes T ON T.ID_THEME = Books.ID_THEME
+            WHERE NameTheme = theme
+            GROUP BY NameTheme
+        )
+    );
+END |
+
+SELECT *
+FROM Books
+WHERE ID_BOOK = getExpensiveBookByTheme('Programming');
+
 # 5.Написать функцию, которая выводит информацию о количестве авторов,
 # живущих в разных странах (название страны передается в качестве параметра).
+CREATE FUNCTION getNumberAuthorsByCountry(country VARCHAR(30))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    RETURN (
+        SELECT COUNT(*)
+        FROM Authors
+                 JOIN Country C ON C.ID_COUNTRY = Authors.ID_COUNTRY
+        WHERE C.NameCountry = country
+    );
+END |
+
+SELECT getNumberAuthorsByCountry('USA');
+
 # 6 Функцию, которая возвращает количество магазинов, которые не продали ни одной книги издательства.
+CREATE FUNCTION getCountShopsWithNoSales()
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    RETURN (
+        SELECT COUNT(*)
+        FROM Shops
+                 LEFT JOIN Sales S ON Shops.ID_SHOP = S.ID_SHOP
+        WHERE ID_SALE IS NULL
+    );
+END;
+|
+
+SELECT getCountShopsWithNoSales();
